dd485ed4bac2d500d7d2eb0098988ec4ed34aacf
hackash99 <111279451+hackash99@users.noreply.github.com>
2022-08-18 18:10:13 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,21 @@
 node default {
+file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+    }
 }
 node 'master.puppet.vm' {
   include role::master_server
   file { '/root/README':
     ensure => file,
     content => $fqdn,
+    owner => 'root',
 }
-
 node /^web/ {
   include role::app_server
   }
-
+  
 node /^db/ {
   include role::db_server
   }
